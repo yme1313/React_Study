3121c4485b34680ea7d0e432a844fc860102c8c2
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-06-13 10:58:51 +0900

    123

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,21 +1,30 @@
-//4.3 함수형 컴포넌트 구현까지 진행
 import React, { useState } from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setUsername(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      form,
+      [e.target.name] : e.target.value
+    }
+    setForm(nextForm);
+  } 
   const onClick = () => {
-    alert(username + ' : ' + message);
-    setUsername('');
-    setMessage('');
-  }
+    alert(username + " : " + message)
+    setForm({
+      username : '',
+      message : ''
+    });
+  } 
   const onKeyPress = e => {
-    if(e.key === 'Enter') {
+    if(e.key === 'Enter'){
       onClick();
     }
-  };
+  }
   return(
     <div>
       <h1>이벤트 연습</h1>
@@ -24,14 +33,14 @@ const EventPractice = () => {
         name="username"
         placeholder="사용자명"
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         type="text"
         name="message"
         placeholder="아무거나 입력해 보세요"
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
