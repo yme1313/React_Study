4df1d4e888f7c20bcd93ecd7afba37f6370c68c9
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-06-16 17:13:08 +0900

    React : 13.6.4 Navigate 학습차례

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,14 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import Articles from './pages/Articles';
+import Article from './pages/Article';
 import Home from './pages/Home';
 import Profile from './pages/profile';
+import Layout from './Layout';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
