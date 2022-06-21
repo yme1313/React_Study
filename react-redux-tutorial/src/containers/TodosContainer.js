fa9323ba97c9cd3af85b68148e66ee9cd4fb5260
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-06-21 22:45:59 +0900

    React : 18장 학습차례

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,36 +1,30 @@
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { changeInput,insert,toggle,remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useActions from '../lib/useActions';
 
-const TodosContainer = ({
-	input,
-	todos,
-	changeInput,
-	insert,
-	toggle,
-	remove
-}) => {
-	return (
-		<Todos
-			input={input}
-			todos={todos}
-			onChangeInput={changeInput}
-			onInsert={insert}
-			onToggle={toggle}
-			onRemove={remove}
-		/>	
-	);
-};
-
-export default connect(
-	({ todos }) => ({
+const TodoContainer = () => {
+	const { input, todos } = useSelector(({ todos }) => ({
 		input : todos.input,
 		todos : todos.todos
-	}),
-	{
-		changeInput,
-		insert,
-		toggle,
-		remove,
-	}
-)(TodosContainer);
+	}));
+
+	const [onChangeInput, onInsert, onToggle, onRemove] = useActions(
+		[changeInput, insert, toggle, remove],
+		[]
+	);
+
+	return (
+		<Todos	
+			input={input}
+			todos={todos}
+			onChangeInput={onChangeInput}
+			onInsert={onInsert}
+			onToggle={onToggle}
+			onRemove={onRemove}
+		/>
+	)
+}
+
+export default React.memo(TodoContainer);
