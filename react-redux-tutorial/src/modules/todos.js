ddd5906938811cd98f4175f5b9c830d937d61f28
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-06-21 17:44:04 +0900

    React : 17.7.4 학습차례

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,32 +1,22 @@
+import { createAction, handleActions } from "redux-actions";
+import produce from 'immer';
+
 const CHAGNE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
-export const changeInput = input => ({
-  tpye : CHAGNE_INPUT,
-  input
-})
+export const changeInput = createAction(CHAGNE_INPUT, input => input);
 
 let id = 3;
-export const insert = text => ({
-  type : INSERT,
-  todo : {
-    id: id++,
-    text,
-    done : false
-  }
-});
+export const insert = createAction(INSERT, text => ({
+  id: id++,
+  text,
+  done: false,
+}))
 
-export const toggle = id => ({
-  type : TOGGLE,
-  id
-})
-
-export const remove = id => ({
-  type : REMOVE,
-  id
-});
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
 
 const intialState = {
   input : '',
@@ -44,32 +34,28 @@ const intialState = {
   ]
 };
 
-function todos(state = intialState, action) {
-  switch (action.type) {
-    case CHAGNE_INPUT:
-      return {
-        ...state,
-        input : action.input
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos : state.todos.concat(action.todo)
-      }  
-    case TOGGLE:
-      return {
-        ...state,
-        todos : state.todos.map(todo =>
-          todo.id === action.id ? {...todo, done: !todo.done} : todo)
-      } 
-    case REMOVE:
-      return {
-        ...state,
-        todo : state.todos.filter(todo => todo.id !== action.id)
-      }  
-    default:
-      return state;  
-  }
-}
+const todos = handleActions(
+  {
+    [CHAGNE_INPUT]: (state, { payload : input }) => 
+      produce(state,draft => {
+        draft.input = input
+      }),
+    [INSERT]: (state, { payload : todo }) => 
+      produce(state,draft => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload : id }) =>
+      produce(state,draft => {
+        const todo = draft.todos.find(todo => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload : id }) => 
+      produce(state,draft => {
+        const index = draft.todos.findIndex(todo => todo.id === id);
+        draft.todos.splice(index, 1);
+      })
+  },
+  intialState
+)
 
 export default todos;
