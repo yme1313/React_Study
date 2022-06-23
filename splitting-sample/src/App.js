c56fe4cd76d078791739995f9257aaeb8f1dda56
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-06-23 22:02:44 +0900

    React : 21장 koa 프레임워크 학습 차례

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { useState, Suspense } from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-const SplitMe = React.lazy(() => import('./SplitMe'))
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+  fallback : <div>loading...</div>
+})
 
 function App() {
   const [visiable, setVisiable] = useState(false);
   const onClick = () => {
     setVisiable(true);
   } 
+  const onMouseOver = () => {
+    SplitMe.preload();
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
+        <p onClick={onClick} onMouseOver={onMouseOver}>Hello React!</p>
           {visiable && <SplitMe />}
-        </Suspense>
       </header>
     </div>
   );
