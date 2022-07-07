edd245820e83115fb7a6558ea94708624b6f6ed8
yme1313 <83192170+yme1313@users.noreply.github.com>
2022-07-07 17:09:02 +0900

    React : 25.2 write_redux 작성 진행

--- blog/blog-frontend/src/components/write/TagBox.js
+++ blog/blog-frontend/src/components/write/TagBox.js
@@ -60,26 +60,62 @@ const TagListBlock = styled.div`
 `;
 
 // React.memo를 사용하여 tag 값이 바뀔 때만 리렌더링되도록 처리
-const TagItem = React.memo(({ tag }) => <Tag>#{tag}</Tag>);
+const TagItem = React.memo(({ tag, onRemove }) => (
+	<Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
 
 // React.memo를 사용하여 tags 값이 바뀔 때만 리렌더링되도록 처리
-const TagList = React.memo(({ tags }) => (
+const TagList = React.memo(({ tags, onRemove }) => (
 	<TagListBlock>
 		{tags.map(tag => (
-			<TagItem key={tag} tag={tag} />
+			<TagItem key={tag} tag={tag} onRemove={onRemove} />
 		))}
 	</TagListBlock>
 ));
 
 const TagBox = () => {
+	const [input, setInput] = useState('');
+	const [localTags, setLocalTags] = useState([]);
+
+	const insertTag = useCallback(
+		tag => {
+			if (!tag) return;
+			if (localTags.includes(tag)) return;
+			setLocalTags([...localTags, tag]);
+		},
+		[localTags]
+	)
+	const onRemove = useCallback(
+		tag => {
+			setLocalTags(localTags.filter(t => t !== tag));
+		},
+		[localTags]
+	)
+
+	const onChange = useCallback(e => {
+		setInput(e.target.value);
+	},[])
+
+	const onSubmit = useCallback(e => {
+			e.preventDefault();
+			insertTag(input.trim());
+			setInput('');
+		},
+		[input, insertTag]
+	)
+
 	return (
 		<TagBoxBlock>
 			<h4>태그</h4>
-			<TagForm>
-				<input placeholder='태그를 입력하세요' />
+			<TagForm onSubmit={onSubmit}>
+				<input 
+					placeholder='태그를 입력하세요' 
+					value={input}
+					onChange={onChange}
+				/>
 				<button type='submit'>추가</button>
 			</TagForm>
-			<TagList tags={['태그1', '태그2', '태그3']} />
+			<TagList tags={localTags} onRemove={onRemove} />
 		</TagBoxBlock>
 	)
 }
--- blog/blog-frontend/src/pages/WritePage.js
+++ blog/blog-frontend/src/pages/WritePage.js
@@ -1,12 +1,14 @@
 import Editor from "../components/write/Editor";
 import TagBox from "../components/write/TagBox";
 import Responsive from "../components/common/Responsive";
+import WriteActionButtons from "../components/write/WriteActionButtons";
 
 const WritePage = () => {
 	return (
 		<Responsive>
 			<Editor />
 			<TagBox />
+			<WriteActionButtons />
 		</Responsive>
 	)
 }
